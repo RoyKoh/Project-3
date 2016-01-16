ec607b5880efe0f55e5e98fcb4bf1a35431042db
RoyKoh <i168168@yahoo.com.sg>
2016-01-16 21:01:30 +0800

    modi app.js

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-/* SERVER */
-var express = require('express')
-var app = express()
-app.use(express.static(__dirname + '/app'))
-var port = process.env.PORT || 5000
-app.listen(port)
+var PORT
+PORT = process.env.PORT || 5000
+const express = require('express')
+const app = express()
+var http = require('http').Server(app)
+ // app.get('/', function(req, res){
+ // res.sendFile(dirname + '/index.html');
+ // });
+var root = __dirname + '/app'
+app.use(express.static(root))
+ //   io.on('connection', function (socket) {
+ //     socket.on('chat message', function (msg) {
+ //     console.log('index => ', msg)
+ //     io.emit('chat message', msg)
+ //     })
+ //   })
+  //
+  // app.use("/bower_components", express.static(__dirname + "/bower_components"))
+http.listen(PORT, () => {
+  console.log(`Listening to http://localhost:${PORT}`)
+})
